8fa03ed813e5799d83f635ec2a7e4776b9377775
Ayaz <ayaz.stjit@gmail.com>
2023-01-22 16:21:55 +0530

    Section-6: DataSource demo

--- Terraform Concept Building Block/Demo DataSource/createInstance.tf
+++ Terraform Concept Building Block/Demo DataSource/createInstance.tf
@@ -1,7 +1,13 @@
+# It will fetch all the availability zones in the region ec2 is created
+data "aws_availability_zones" "available" {
+  
+}
+
 resource "aws_instance" "MyFirstInstance" {
 	ami = lookup(var.AMIS, var.AWS_REGION) #"ami-05803413c51f242b7"
 	instance_type = "t2.micro"
-
+	availability_zone = data.aws_availability_zones.available.names[0]
+	
 	tags = {
 		Name = "custom_instance"
 	}
