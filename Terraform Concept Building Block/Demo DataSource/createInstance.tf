2736c2f6039b76b727e478d1e6a16e2fd11c3a8e
Ayaz <ayaz.stjit@gmail.com>
2023-01-28 23:27:04 +0530

    Section-6: use data for ami

--- Terraform Concept Building Block/Demo DataSource/createInstance.tf
+++ Terraform Concept Building Block/Demo DataSource/createInstance.tf
@@ -1,13 +1,27 @@
 # It will fetch all the availability zones in the region ec2 is created
-data "aws_availability_zones" "available" {
-  
+data "aws_availability_zones" "available" { }
+
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+
+  filter {
+	name = "name"
+	values =["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+	name = "virtualization-type"
+	values = ["hvm"]
+  }
+
 }
 
 resource "aws_instance" "MyFirstInstance" {
-	ami = lookup(var.AMIS, var.AWS_REGION) #"ami-05803413c51f242b7"
+	ami = data.aws_ami.latest-ubuntu.id #lookup(var.AMIS, var.AWS_REGION) #"ami-05803413c51f242b7"
 	instance_type = "t2.micro"
 	availability_zone = data.aws_availability_zones.available.names[0]
-	
+
 	tags = {
 		Name = "custom_instance"
 	}
