0113756458ca5e90d0823ffa09164fa4b3f4c413
Ayaz <ayaz.stjit@gmail.com>
2023-01-28 23:45:09 +0530

    Section-6: Remove ami from variables

--- Terraform Concept Building Block/Demo DataSource/variables.tf
+++ Terraform Concept Building Block/Demo DataSource/variables.tf
@@ -14,8 +14,8 @@ variable "Security_Group" {
 variable "AMIS" {
 	type = map
 	default = {
-		us-east-1 = "ami-0b0ea68c435eb488d"
-		us-east-2 = "ami-05803413c51f242b7"
+		# us-east-1 = "ami-0b0ea68c435eb488d"
+		# us-east-2 = "ami-05803413c51f242b7"
 		us-west-1 = "ami-0454207e5367abf01"
 		us-west-2 = "ami-0688ba7eeeeefe3cd"
 	}
